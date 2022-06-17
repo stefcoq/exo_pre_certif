3ec0d5db4517653018dcf544fe85c2a4d136be42
Benjamin Boisson <91035587+BetterCallBen@users.noreply.github.com>
2022-06-17 14:48:56 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show]
-  get '/my_profile', to: 'users#my_profile', as: 'my_profile'
-  resources :trees do
-    resources :adoptions, only: [:new, :create, :destroy]
-  end
-
-  resources :adoptions, only: [] do
-    resources :reviews, only: [:create]
-  end
 end
